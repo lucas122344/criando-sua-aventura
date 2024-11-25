5724a98ca5393278d3eb428a6be8f0420fda4897
lucas122344 <ltulio769@gmail.com>
2024-11-25 19:18:51 -0300

    projeto final

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const avanca = document.querySelectorall('.btn-proximo')
+const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click', function{
-        const atual = document.queryselector('.ativo');
-        const proximopasso = 'passo-' + this.getAttribute('data-proximo');
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
         atual.classList.remove('ativo');
-        document.getElementById(proximopasso).classList.add('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
 
